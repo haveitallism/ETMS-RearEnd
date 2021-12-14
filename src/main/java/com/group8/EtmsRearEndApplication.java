ad861be75bb4f81991a01f5d1bdee8a8c1fdca42
laiyong <cdlywork@163.com>
2021-12-14 14:52:53 +0800

    修改配置，测试运行情况

--- src/main/java/com/group8/EtmsRearEndApplication.java
+++ src/main/java/com/group8/EtmsRearEndApplication.java
@@ -1,9 +1,11 @@
 package com.group8;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.group8.dao")
 public class EtmsRearEndApplication {
 
     public static void main(String[] args) {
