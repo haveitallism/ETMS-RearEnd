a71761971eaf167183c829d07be02ef40abeb970
acoffee <1427731607@qq.com>
2021-12-15 10:54:07 +0800

    个人信息的查看和修改以及密码的修改

--- src/main/java/com/group8/entity/User.java
+++
@@ -1,8 +0,0 @@
-package com.group8.entity;
-
-/**
- * @author QY
- * @create 2021-12-14 14:21
- */
-public class User {
-}
---
+++ src/main/java/com/group8/service/UserService.java
@@ -0,0 +1,21 @@
+package com.group8.service;
+
+import com.group8.entity.EtmsUser;
+
+import java.util.List;
+
+/**
+ * @author acoffee
+ * @create 2021-12-14 15:56
+ */
+public interface UserService {
+    List<EtmsUser> findAllUser();
+
+    EtmsUser findUserById(int id);
+
+    boolean updateUser( EtmsUser etmsUser);
+
+    boolean updatePassword(int id,String newPassword);
+
+    boolean validatePassword(int id,String oldPassword);
+}
