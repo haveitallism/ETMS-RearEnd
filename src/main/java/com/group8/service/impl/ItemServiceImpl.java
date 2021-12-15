e74638e226ded8ee5e19f82aa27b1828653018d6
qiyu <454476569@qq.com>
2021-12-15 16:33:14 +0800

    新增：1.新建培训项目 2.根据关键字查询培训项目分页查询

--- src/main/java/com/group8/service/impl/ItemServiceImpl.java
+++ src/main/java/com/group8/service/impl/ItemServiceImpl.java
@@ -32,52 +32,51 @@ public class ItemServiceImpl implements ItemService {
     @Override
     public int update(EtmsItem etmsItem) {
         return itemDao.update(etmsItem);
-
+    }
     /*
     添加培训项目 从DTO从取得3个对象
      */
-    @Override
-    public int addItem(EtmsItemAbilityOutline iao) {
+        @Override
+        public int addItem (EtmsItemAbilityOutline iao){
 
-        int i2 = 0;
-        int i3 = 0;
+            int i2 = 0;
+            int i3 = 0;
 
-        //添加培训项目
-        EtmsItem etmsItem = iao.getEtmsItem();
-        int i1 = itemDao.addOne(etmsItem);
+            //添加培训项目
+            EtmsItem etmsItem = iao.getEtmsItem();
+            int i1 = itemDao.addOne(etmsItem);
 
-        List<EtmsOutline> etmsOutlines = iao.getEtmsOutlines();
-        for(EtmsOutline eoi:etmsOutlines) {
-            i2 = outlineDao.addOne(eoi);
-            //如果中间添加失败 则中断循环
-            if (i2 < 0) {
-                break;
+            List<EtmsOutline> etmsOutlines = iao.getEtmsOutlines();
+            for (EtmsOutline eoi : etmsOutlines) {
+                i2 = outlineDao.addOne(eoi);
+                //如果中间添加失败 则中断循环
+                if (i2 < 0) {
+                    break;
+                }
+            }
+
+            //循环添加能力模型
+            List<EtmsItemAm> list = iao.getItemAmLists();
+            for (EtmsItemAm eia : list) {
+                i3 = abilityDao.addOne(eia);
+                //如果中间添加失败 则中断循环
+                if (i3 < 0) {
+                    break;
+                }
+            }
+            //如果其中一项不大于0 则添加失败
+            if (i1 > 0 && i2 > 0 && i3 > 0) {
+                return 1;
+            } else {
+                return 0;
             }
         }
-
-        //循环添加能力模型
-        List<EtmsItemAm> list = iao.getItemAmLists();
-        for (EtmsItemAm eia:list) {
-            i3 = abilityDao.addOne(eia);
-            //如果中间添加失败 则中断循环
-            if(i3 < 0){
-                break;
-            }
-        }
-
-        //如果其中一项不大于0 则添加失败
-        if (i1 > 0 && i2 > 0 && i3 > 0){
-            return 1;
-        }else {
-            return 0;
+        @Override
+        public List<EtmsItem> findItem (EtmsItem etmsItem){
+            System.out.println("123" + etmsItem);
+            List<EtmsItem> list = itemDao.findItem(etmsItem);
+            System.out.println(list);
+            return list;
         }
     }
 
-    @Override
-    public List<EtmsItem> findItem(EtmsItem etmsItem) {
-        System.out.println("123"+etmsItem);
-        List<EtmsItem> list = itemDao.findItem(etmsItem);
-        System.out.println(list);
-        return list;
-    }
-}
