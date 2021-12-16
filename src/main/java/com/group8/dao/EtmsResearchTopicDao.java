5960f66f792e326d332d9a8ec17521a3bca16ee0
zl <1147959718>
2021-12-16 10:48:38 +0800

    新增

--- src/main/java/com/group8/dao/EtmsResearchTopicDao.java
+++ src/main/java/com/group8/dao/EtmsResearchTopicDao.java
@@ -1,9 +1,12 @@
 package com.group8.dao;
 
+import com.group8.entity.EtmsResachAnwer;
 import com.group8.entity.EtmsResachTopic;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface EtmsResearchTopicDao {
-    List<EtmsResachTopic> findAll(List topics);
+    List<EtmsResachTopic> findTopic(@Param("sss") List topics);
+    List<EtmsResachAnwer> findAnswer(List answers);
 }
--- src/test/java/com/group8/EtmsRearEndApplicationTests.java
+++ src/test/java/com/group8/EtmsRearEndApplicationTests.java
@@ -2,10 +2,12 @@ package com.group8;
 
 import com.group8.dao.EtmsResearchAnswerDao;
 import com.group8.dao.EtmsResearchTopicDao;
+import com.group8.entity.EtmsResachTopic;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
@@ -16,9 +18,12 @@ EtmsResearchTopicDao etmsResearchTopic;
     EtmsResearchAnswerDao etmsResearchAnswerDao;
     @Test
     void contextLoads() {
-        List all = etmsResearchAnswerDao.findAll();
-        System.out.println(all);
-        System.out.println(etmsResearchTopic.findAll(all));
+        List s = new ArrayList();
+        s.add(1);
+        s.add(2);
+        s.add(3);
+        List<EtmsResachTopic> topic = etmsResearchTopic.findTopic(s);
+        System.out.println(topic);
 
     }
 
